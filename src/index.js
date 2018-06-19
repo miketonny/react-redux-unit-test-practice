56a63cc5389b9f1768dc081c1750909aa4d85e07
miketonny <miketonny@gmail.com>
2018-06-20 11:56:35 +1200

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import store from './configureStore';
+
+
 import App from "./App.js";
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById("root")
 );
